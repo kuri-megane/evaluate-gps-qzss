16736aa287b39b3d9243dd07e000406db617519b
kuri.megane my_ubuntu <kuri.megane3060.4@gmail.com>
2019-08-18 03:31:53 +0900

    [FIX] リファクタリング

--- upload_dropbox.sh
+++ upload_dropbox.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 sleep 3m 
-
-## config
 TODAY=$(date "+%Y%m%d")
-
-## web backup
 zip $HOME/log-$TODAY-pi.zip $HOME/evaluate-gps-qzss/data/*
-## backup to dropbox
 $HOME/Dropbox-Uploader/dropbox_uploader.sh upload $HOME/log-$TODAY-pi.zip .
 rm $HOME/log-$TODAY-pi.zip
